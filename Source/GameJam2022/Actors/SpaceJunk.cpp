363a62f589add57ed589be5d682d5c12a1e187da
Daphoeno <38322740+Daphoeno@users.noreply.github.com>
2022-02-13 12:45:17 +0000

    final comit

--- Source/GameJam2022/Actors/SpaceJunk.cpp
+++ Source/GameJam2022/Actors/SpaceJunk.cpp
@@ -354,6 +354,7 @@ void ASpaceJunk::UpdatePosition()
 			DebugMessage.Append(FString::SanitizeFloat(GravityRatio));
 			GEngine->AddOnScreenDebugMessage(-1, 0.f, FColor::Cyan, DebugMessage);
 		}
+#endif
 
 		TargetLocation = (GetActorLocation() + (PlayerDirection));
 
@@ -363,7 +364,6 @@ void ASpaceJunk::UpdatePosition()
 
 		SetActorLocation(FVector(LerpLocation.X, LerpLocation.Y, 0.f));
 	}
-#endif
 }
 
 void ASpaceJunk::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
